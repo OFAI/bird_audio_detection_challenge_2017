7c65dc49fd12393bf64ba3e03eafd85c688c8d72
Thomas Grill <thomas.grill@ofai.at>
2017-01-30 23:05:40 +0100

    spectrograms using full sample rate

--- code/prepare_spectrograms.sh
+++ code/prepare_spectrograms.sh
@@ -13,7 +13,7 @@ do
     o="$SPECT/$sp/${b}.h5"
     if [ ! -f "$o" ]; then
         echo "Making ${o}"
-        if ! $here/extract_melspect.py --channels=mix-after -r 22050 -f 70 -l 1024 -t mel -m 50 -M 11000 -b 80 -s log --featname "features" --include-times --times-mode=borders "$f" "$o"; then
+        if ! $here/extract_melspect.py --channels=mix-after -r 44100 -f 70 -l 2048 -t mel -m 50 -M 11000 -b 80 -s log --featname "features" --include-times --times-mode=borders "$f" "$o"; then
             echo "Failed making ${o} - exiting"
             return $?
         fi
