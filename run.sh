98650c74fb2df9ad5f65cadc817858ff829a7af7
Thomas Grill <thomas.grill@ofai.at>
2017-02-08 16:08:11 +0100

    Include validation run

--- run.sh
+++ run.sh
@@ -64,7 +64,7 @@ function train_model {
     "$here/code/simplenn_main.py" \
     --mode=train \
     --problem=binary \
-    --var measures= \
+    --var measures=auroc \
     --inputs filelist:filelist \
     --var filelist:path="$LISTPATH" \
     --var filelist:lists="${filelists}" \
@@ -78,6 +78,7 @@ function train_model {
     --process collect:collect \
     --var "collect:source=0..1"  \
     --process "scale@1:range(out_min=0.01,out_max=0.99)" \
+    --process "async(queue_size=256)" \
     --layers "${net_layers}" \
     --save "${model}.h5" \
     ${net_options} \
@@ -152,7 +153,7 @@ function stage1_train {
         model="$WORKPATH/model_first_${i}"
         if [ ! -f "${model}.h5" ]; then # check for existence
             echo_status "Training model ${model}."
-            train_model "${model}" "train_${i}" '' ${i} ${cmdargs} || return $?
+            train_model "${model}" "train_${i},val_${i}" '' ${i} ${cmdargs} || return $?
             echo_status "Done training model ${model}."
         else
             echo_status "Using existing model ${model}."
@@ -235,7 +236,7 @@ function stage2_train {
             model="$WORKPATH/model_second_${i}_${h}"
             if [ ! -f "${model}.h5" ]; then # check for existence
                 echo_status "Training model ${model}."
-                train_model "${model}" "train_${i}_pseudo_${h}" "$LISTPATH/testdata.pseudo_*" ${i} ${cmdargs} || return $?
+                train_model "${model}" "train_${i}_pseudo_${h},val_${i}" "$LISTPATH/testdata.pseudo_*" ${i} ${cmdargs} || return $?
                 echo_status "Done training model ${model}."
             else
                 echo_status "Using existing model ${model}."
