4393ded7dda27302054882e0e21fba7309e97437
Thomas Grill <thomas.grill@ofai.at>
2017-02-17 11:25:11 +0100

    Updated validation

--- run.sh
+++ run.sh
@@ -129,6 +129,38 @@ function stage1_prepare {
 
     echo_status "Done computing spectrograms."
 
+<<<<<<< HEAD
+=======
+    clusterfile="${WORKPATH}/clusters-train.h5"
+    if [ ! -f "${clusterfile}" ]; then
+        echo_status "Computing training data clusters."
+        datasets=${TRAIN// /,}
+        dataspects=`for t in $TRAIN; do echo "${SPECTPATH}"/$t/'*.wav.h5'; done`
+        "$here/code/cluster_spects.py" ${dataspects} --datasets ${datasets} --skipframes=35 --pca=0.95 --clusters=${TRAIN_CLUSTERS} --outfile "${clusterfile}"
+    else
+        echo_status "Using existing predictions ${clusterfile}."
+    fi
+
+    clusterfile="${WORKPATH}/clusters-test.h5"
+    if [ ! -f "${clusterfile}" ]; then
+        echo_status "Computing testing data clusters."
+        datasets=${TEST// /,}
+        dataspects=`for t in $TEST; do echo "${SPECTPATH}"/$t/'*.wav.h5'; done`
+        "$here/code/cluster_spects.py" ${dataspects} --datasets ${datasets} --skipframes=35 --pca=0.95 --clusters=${TEST_CLUSTERS} --outfile "${clusterfile}"
+    else
+        echo_status "Using existing predictions ${clusterfile}."
+    fi
+
+    echo_status "Preparing file lists."
+    mkdir $LISTPATH 2> /dev/null
+
+    # file list for training set
+    "$here/code/create_filelists.py" "$LABELPATH" ${TRAIN} --out "$LISTPATH/%(fold)s_%(num)i" --num ${model_count} --folds "train=$((model_count-1)),val=1" || return $?
+
+    # file lists for test set, one per cluster
+    "$here/code/create_filelists.py" "$LABELPATH" ${TEST} --out "$LISTPATH/%(fold)s_%(cluster)i" --num ${model_count} --folds "test=1" --clusterfile "${WORKPATH}/clusters-test.h5" || return $?
+
+>>>>>>> 364f399... Updated validation
     email_status "Done with stage1 preparations" "Computed filelists and spectrograms."
 }
 
@@ -207,7 +239,7 @@ function stage1_validate {
     # prediction by bagging
     echo_status "Bagging first stage validations."
     vallists=`echo $LISTPATH/val_?`
-    "$here/code/predict.py" "$WORKPATH"/model_first_?.validation.h5 --filelist ${vallists// /,} --out "$first_validations" --keep-prefix --keep-suffix --out-header || return $?
+    "$here/code/predict.py" "$WORKPATH"/model_first_?.validation.h5 --filelist ${vallists// /,} --out "$first_validations" --keep-prefix --keep-suffix --out-header --skip-missing || return $?
     filelists=""
     for t in ${TRAIN}; do
         filelists+=" ${LABELPATH}/${t}.csv"
